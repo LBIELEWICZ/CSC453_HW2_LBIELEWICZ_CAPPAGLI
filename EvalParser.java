a77d6efa7979755c1d78777400adfaccd5cbb85c
FyraFlare <cappagli@email.arizona.edu>
2019-09-25 23:07:57 -0700

    Helper functions now evaluate, and fixed not passing args in them.

--- EvalParser.java
+++ EvalParser.java
@@ -20,47 +20,52 @@ public class EvalParser {
   /* TODO #1: Write a parser that can evaluate expressions */
   public int evaluateExpression(String eval){
     LinkedList<Token> tokens = scan.extractTokenList(eval);
-    //todo
+    int result = //todo
     return 0;
   }
 
-  // Parsing E/E'
-  private void E(LinkedList<Token> tokens){
-    T();
+  // Evaluating E/E'
+  private int evaluateE(LinkedList<Token> tokens){
+    int r;
+    r = T(tokens);
     while(true){ // E'
       if(tokens.peek().tokenType == TokenType.PLUS){
         tokens.remove(); //match('+');
-        T();
+        r = r + T(tokens);
       }else if(tokens.peek().tokenType == TokenType.MINUS){
         tokens.remove(); //match('-');
-        T();
+        r = r - T(tokens);
       }else{
         break;
       }
     }
+    return r;
   }
 
-  // Parsing T/T'
-  private void T(LinkedList<Token> tokens){
-    F();
+  // Evaluating T/T'
+  private int evaluateT(LinkedList<Token> tokens){
+    int r;
+    r = F(tokens);
     while(true){ // T'
       if(tokens.peek().tokenType == TokenType.MUL){
         tokens.remove(); //match('*');
-        F();
+        r = r * F(tokens);
       }else if(tokens.peek().tokenType == TokenType.DIV){
         tokens.remove(); //match('/');
-        F();
+        r = r / F(tokens);
       }else{
         break;
       }
     }
+    return r;
   }
 
-  // Parsing F
-  private void F(LinkedList<Token> tokens){
+  // Evaluating F
+  private int evaluateF(LinkedList<Token> tokens){
+    int r;
     if(tokens.peek().tokenType == TokenType.OP){
       tokens.remove(); //match('(');
-      E();
+      r = E(tokens);
       if(tokens.peek().tokenType == TokenType.CP){
         tokens.remove(); //match(')');
       }else{
@@ -68,11 +73,12 @@ public class EvalParser {
         System.exit(1);
       }
     }else if(tokens.peek().tokenType == TokenType.NUM){
-      tokens.remove(); //match(number);
+      r = tokens.remove().tokenVal; //match(number);
     }else{
        System.out.println("ERROR: Not in the grammer.");
        System.exit(1);
     }
+    return r;
   }
 
   /* TODO #2: Now add three address translation to your parser*/
